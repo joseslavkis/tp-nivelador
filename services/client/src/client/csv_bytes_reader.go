7043fda72fc0b8160ea002e6dd1a01352e450a12
joseslavkis <joseslavkis@gmail.com>
2026-09-02 12:12:25 -0300

    deleted quote logic from reader

--- services/client/src/client/csv_bytes_reader.go
+++ services/client/src/client/csv_bytes_reader.go
@@ -12,12 +12,9 @@ const (
 	maxCSVRecordSize = 512 * 1024
 )
 
-var errUnterminatedQuotedField = errors.New("unterminated quoted field")
-
 type csvBytesReader struct {
 	reader       *bufio.Reader
 	recordBuffer []byte
-	quotedFields []byte
 }
 
 func newCSVBytesReader(reader io.Reader) *csvBytesReader {
@@ -53,73 +50,28 @@ func (reader *csvBytesReader) Read() ([csvFieldCount][]byte, error) {
 			reader.recordBuffer = reader.recordBuffer[:0]
 			continue
 		}
-		fields, parseErr := reader.parseRecord(record)
-		if errors.Is(parseErr, errUnterminatedQuotedField) && !errors.Is(readErr, io.EOF) {
-			continue
-		}
-		return fields, parseErr
+		return reader.parseRecord(record)
 	}
 }
 
 func (reader *csvBytesReader) parseRecord(record []byte) ([csvFieldCount][]byte, error) {
 	var fields [csvFieldCount][]byte
-	reader.quotedFields = reader.quotedFields[:0]
 	fieldIndex := 0
-	offset := 0
+	fieldStart := 0
 
-	for {
-		if fieldIndex == csvFieldCount {
+	for offset, value := range record {
+		if value != ',' {
+			continue
+		}
+		if fieldIndex == csvFieldCount-1 {
 			return fields, fmt.Errorf("record contains more than %d fields", csvFieldCount)
 		}
-
-		if offset < len(record) && record[offset] == '"' {
-			quotedStart := len(reader.quotedFields)
-			offset++
-			closed := false
-			for offset < len(record) {
-				if record[offset] == '\r' && offset+1 < len(record) && record[offset+1] == '\n' {
-					reader.quotedFields = append(reader.quotedFields, '\n')
-					offset += 2
-					continue
-				}
-				if record[offset] != '"' {
-					reader.quotedFields = append(reader.quotedFields, record[offset])
-					offset++
-					continue
-				}
-				if offset+1 < len(record) && record[offset+1] == '"' {
-					reader.quotedFields = append(reader.quotedFields, '"')
-					offset += 2
-					continue
-				}
-				offset++
-				closed = true
-				break
-			}
-			if !closed {
-				return fields, errUnterminatedQuotedField
-			}
-			if offset < len(record) && record[offset] != ',' {
-				return fields, fmt.Errorf("unexpected character after closing quote")
-			}
-			fields[fieldIndex] = reader.quotedFields[quotedStart:]
-		} else {
-			fieldStart := offset
-			for offset < len(record) && record[offset] != ',' {
-				if record[offset] == '"' {
-					return fields, fmt.Errorf("unexpected quote in unquoted field")
-				}
-				offset++
-			}
-			fields[fieldIndex] = record[fieldStart:offset]
-		}
-
+		fields[fieldIndex] = record[fieldStart:offset]
 		fieldIndex++
-		if offset == len(record) {
-			break
-		}
-		offset++
+		fieldStart = offset + 1
 	}
+	fields[fieldIndex] = record[fieldStart:]
+	fieldIndex++
 
 	if fieldIndex != csvFieldCount {
 		return fields, fmt.Errorf("record contains %d fields, expected %d", fieldIndex, csvFieldCount)
